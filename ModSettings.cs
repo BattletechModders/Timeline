bb5f7606cfca2f69db915ad47d14afc0ac1b04ca
gnivler <gnivil@gmail.com>
2020-01-01 15:23:35 -0600

    Merge branch 'master' of N:\Repos\Timeline with conflicts.

--- ModSettings.cs
+++ ModSettings.cs
@@ -7,6 +7,7 @@ namespace Timeline
     {
         public string DateFormatString = "{0:yyyy MMMM d}{1}";
         public float AdvanceToTaskTime = 0.25f;
+        public bool UseScroller = false;
 
         public static ModSettings ReadSettings(string json)
         {
